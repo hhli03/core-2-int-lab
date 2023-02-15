0a73ee3300e7b0d4572b7e62b3dde96fca2b0eee
hhli03 <lik954@newschool.edu>
2023-02-14 23:22:20 -0500

    page2update

---
+++ projects/project1/page2/script.js
@@ -0,0 +1,65 @@
+console.log('loaded');
+
+$(document).ready(function(){
+
+    $('.spaghettiwhite1').hover(function(){
+        console.log('hover');
+        $('.spaghettired1').addClass('active');
+    });
+
+    $('.spaghettiwhite2').hover(function(){
+        console.log('hover');
+        $('.spaghettired2').addClass('active');
+    });
+
+    $('.spaghettiwhite3').hover(function(){
+        console.log('hover');
+        $('.spaghettired3').addClass('active');
+    });
+
+
+    $('.spaghettiwhite4').hover(function(){
+        console.log('hover');
+        $('.spaghettired4').addClass('active');
+    });
+
+
+    $('.spaghettiwhite5').hover(function(){
+        console.log('hover');
+        $('.spaghettired5').addClass('active');
+    });
+
+
+    $('.spaghettiwhite6').hover(function(){
+        console.log('hover');
+        $('.spaghettired6').addClass('active');
+    });
+
+    $('.spaghettiwhite7').hover(function(){
+        console.log('hover');
+        $('.spaghettired7').addClass('active');
+    });
+    $('.spaghettiwhite8').hover(function(){
+        console.log('hover');
+        $('.spaghettired8').addClass('active');
+    });
+    $('.spaghettiwhite9').hover(function(){
+        console.log('hover');
+        $('.spaghettired9').addClass('active');
+    });
+
+    $('.spaghettiwhite10').hover(function(){
+        console.log('hover');
+        $('.spaghettired10').addClass('active');
+    });
+
+    $('.spaghettiwhite11').hover(function(){
+        console.log('hover');
+        $('.spaghettired11').addClass('active');
+    });
+
+    $('.spaghettiwhite12').hover(function(){
+        console.log('hover');
+        $('.spaghettired12').addClass('active');
+    });
+});
