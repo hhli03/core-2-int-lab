baca5d443e6cb1f9c943fe7e5e039fbf57cfe31a
hhli03 <lik954@newschool.edu>
2023-02-16 21:39:41 -0500

    page1updated2/16

---
+++ projects/project1/page1/script.js
@@ -0,0 +1,16 @@
+console.log('loaded');
+
+$(document).ready(function(){
+
+$( "#red1" ).click(function() {
+        $( "#red1" ).toggleClass('active');
+        });
+
+$( "#red2" ).click(function() {
+        $( "#red2" ).toggleClass('active');
+        });
+
+$( "#red3" ).click(function() {
+        $( "#red3" ).toggleClass('active');
+        });
+});
