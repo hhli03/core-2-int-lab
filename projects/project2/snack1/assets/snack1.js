eda763bbfd1c2cca4af25ba6687e94a05228f88a
hhli03 <lik954@newschool.edu>
2023-03-07 21:25:54 -0500

    update1

--- projects/project2/snack1/assets/snack1.js
+++ projects/project2/snack1/assets/snack1.js
@@ -18,7 +18,7 @@ let snackObject = {
     "story/memory": "This crunchy noodle snack was a staple in many households in Hong Kong, and it was one of my all-time favorites. Whenever I opened a pack of MAMEE Snack Noodles, the first thing I would do is crush the noodles into small pieces inside the packet. Then, I would sprinkle in the seasoning powder and shake everything up until the noodles were evenly coated.\n\nThe result was a savory, crunchy snack that was perfect for satisfying my hunger pangs. The seasoning powder had just the right amount of saltiness and spice, and the texture of the crushed noodles was addictively satisfying.\n\nI loved snacking on MAMEE Snack Noodles during my after-school activities, whether it was hanging out with friends or playing sports. The snack was easy to carry around in my backpack, and I could quickly whip up a serving whenever I needed a quick boost of energy.\n\nAs I grew older, I continued to enjoy MAMEE Snack Noodles as a comforting snack. Whenever I missed the taste of home, I would make sure to stock up on a few packets of the snack."
   };
 //setup container element
-let container = document.getElementById("container");
+let container = document.getElementById("leftbox");
 
 // must setup a local server to use fetch
 // see Python instructions here:
@@ -46,11 +46,8 @@ function processSnacks( data ){
     // newItem.style.cssText = `font-size: ${usage}px`;
     newItem.innerHTML = `
       <!--commenting out <div class="image"><img src="assets/images/${item.img}.jpg"></div>-->
-      <div class="year">${item.snack-name-eng}</div>
-      <div class="category">${item.category}</div>
-      <div class="description">${item.description}</div>
-      <div class="flavors">${item.flavors}</div>`;
-    container.appendChild(newItem);    
+      <div class="year">${item['snack-name-eng']}</div>`;
+    leftbox.appendChild(newItem);    
   });
 }
 
