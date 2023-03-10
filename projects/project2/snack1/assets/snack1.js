6f86aa3d3e596918abf54a4bb41042403e490559
hhli03 <lik954@newschool.edu>
2023-03-09 19:13:23 -0500

    reading7

--- projects/project2/snack1/assets/snack1.js
+++ projects/project2/snack1/assets/snack1.js
@@ -3,19 +3,20 @@
 let letters = ['a', 'b', 'c']; //an array;
 
 let snackObject = {
-    "id": 1,
-    "snack-name-eng": "MAMEE Snack Noodles",
-    "snack-name-chi": "媽咪麵",
-    "cantonese-pronunciation": "maa1mi4 min6",
-    "img": "mamee-snack-noodles.jpeg",
-    "description": "a crunchy noodle snack where you simply crush the noodles up before opening, add in the seasoning powder, and shake along till everything is mixed.",
-    "net-weight": "60g",
-    "category": "snack noodles",
-    "price-usd": "$2.83",
-    "flavors": "original",
-    "brand": "Mamee",
-    "personal-rating (1-5)": 5,
-    "story/memory": "This crunchy noodle snack was a staple in many households in Hong Kong, and it was one of my all-time favorites. Whenever I opened a pack of MAMEE Snack Noodles, the first thing I would do is crush the noodles into small pieces inside the packet. Then, I would sprinkle in the seasoning powder and shake everything up until the noodles were evenly coated.\n\nThe result was a savory, crunchy snack that was perfect for satisfying my hunger pangs. The seasoning powder had just the right amount of saltiness and spice, and the texture of the crushed noodles was addictively satisfying.\n\nI loved snacking on MAMEE Snack Noodles during my after-school activities, whether it was hanging out with friends or playing sports. The snack was easy to carry around in my backpack, and I could quickly whip up a serving whenever I needed a quick boost of energy.\n\nAs I grew older, I continued to enjoy MAMEE Snack Noodles as a comforting snack. Whenever I missed the taste of home, I would make sure to stock up on a few packets of the snack."
+  "id": 1,
+  "snacknameeng": "MAMEE Snack Noodles",
+  "snacknamechi": "媽咪麵",
+  "cantonesepronunciation": "maa1mi4 min6",
+  "origin": "Hong Kong",
+  "img": "mamee-snack-noodles.jpeg",
+  "description": "a crunchy noodle snack where you simply crush the noodles up before opening, add in the seasoning powder, and shake along till everything is mixed.",
+  "netweight": "60g",
+  "category": "snack noodles",
+  "priceusd": "$2.83",
+  "flavors": "original",
+  "brand": "Mamee",
+  "personalrating": 5,
+  "story": "This crunchy noodle snack was a staple in many households in Hong Kong, and it was one of my all-time favorites. Whenever I opened a pack of MAMEE Snack Noodles, the first thing I would do is crush the noodles into small pieces inside the packet. Then, I would sprinkle in the seasoning powder and shake everything up until the noodles were evenly coated.\n\nThe result was a savory, crunchy snack that was perfect for satisfying my hunger pangs. The seasoning powder had just the right amount of saltiness and spice, and the texture of the crushed noodles was addictively satisfying.\n\nI loved snacking on MAMEE Snack Noodles during my after-school activities, whether it was hanging out with friends or playing sports. The snack was easy to carry around in my backpack, and I could quickly whip up a serving whenever I needed a quick boost of energy.\n\nAs I grew older, I continued to enjoy MAMEE Snack Noodles as a comforting snack. Whenever I missed the taste of home, I would make sure to stock up on a few packets of the snack."
   };
 //setup container element
 let container = document.getElementById("leftbox");
@@ -46,9 +47,24 @@ function processSnacks( data ){
     // newItem.style.cssText = `font-size: ${usage}px`;
     newItem.innerHTML = `
       <!--commenting out <div class="image"><img src="assets/images/${item.img}.jpg"></div>-->
-      <div class="year">${item['snack-name-eng']}</div>`;
+      <div class="snacknameeng">${item.snacknameeng}</div>
+      <div class="cantonesepronunciation">${item.cantonesepronunciation}</div>
+      <div class="snacknamechi">${item.snacknamechi}</div>`;
+
     leftbox.appendChild(newItem);    
   });
 }
 
+
+
+ // newItem.style.cssText = `font-size: ${usage}px`;
+    //if setting up new pages /links
+    // newItem.innerHTML = `
+    // <a href="${item.linkname}.html"><div class="category">${item.category}</div>
+    // <div class="emoji">${item.emoji}</div>
+    // <div class="detail">
+    //     ${item.emoji}<br>
+    //     <p class="description">${item.description}</div>
+    // </div></a>
+    // `;
 sayHello();
