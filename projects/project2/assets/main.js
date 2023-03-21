a2dc55f83c7771ef930674cf2f9d8fe139e45719
hhli03 <lik954@newschool.edu>
2023-03-21 15:46:16 -0400

    project2

---
+++ projects/project2/assets/main.js
@@ -0,0 +1,107 @@
+console.log('loaded');
+
+$(document).ready(function(){
+
+    $('#snackimg1').click(function(){
+        $('#snack1name').addClass('active');
+    });
+
+    $('#snackimg2').click(function(){
+        $('#snack2name').addClass('active');
+    });
+
+    $('#snackimg3').click(function(){
+        $('#snack3name').addClass('active');
+    });
+    $('#snackimg4').click(function(){
+        $('#snack4name').addClass('active');
+    });
+    $('#snackimg5').click(function(){
+        $('#snack5name').addClass('active');
+    });
+    $('#snackimg6').click(function(){
+        $('#snack6name').addClass('active');
+    });
+    $('#snackimg7').click(function(){
+        $('#snack7name').addClass('active');
+    });
+        $('#snackimg8').click(function(){
+        $('#snack8name').addClass('active');
+    });
+
+    $('#snackimg9').click(function(){
+        $('#snack9name').addClass('active');
+    });
+    $('#snackimg10').click(function(){
+        $('#snack10name').addClass('active');
+    });
+
+    $('#snackimg11').click(function(){
+        $('#snack11name').addClass('active');
+    });
+
+    $('#snackimg12').click(function(){
+        $('#snack12name').addClass('active');
+    });
+
+    $('#snackimg13').click(function(){
+        $('#snack13name').addClass('active');
+    });
+    $('#snackimg14').click(function(){
+        $('#snack14name').addClass('active');
+    });
+    $('#snackimg15').click(function(){
+        $('#snack15name').addClass('active');
+    });
+    $('#snackimg16').click(function(){
+        $('#snack16name').addClass('active');
+    });
+    $('#snackimg17').click(function(){
+        $('#snack17name').addClass('active');
+    });
+        $('#snackimg18').click(function(){
+        $('#snack18name').addClass('active');
+    });
+
+    $('#snackimg19').click(function(){
+        $('#snack19name').addClass('active');
+    });
+    $('#snackimg20').click(function(){
+        $('#snack20name').addClass('active');
+    });
+
+    $('#snackimg21').click(function(){
+        $('#snack21name').addClass('active');
+    });
+
+    $('#snackimg22').click(function(){
+        $('#snack22name').addClass('active');
+    });
+
+    $('#snackimg23').click(function(){
+        $('#snack23name').addClass('active');
+    });
+    $('#snackimg24').click(function(){
+        $('#snack24name').addClass('active');
+    });
+    $('#snackimg25').click(function(){
+        $('#snack25name').addClass('active');
+    });
+    $('#snackimg26').click(function(){
+        $('#snack26name').addClass('active');
+    });
+    $('#snackimg27').click(function(){
+        $('#snack27name').addClass('active');
+    });
+        $('#snackimg28').click(function(){
+        $('#snack28name').addClass('active');
+    });
+
+    $('#snackimg29').click(function(){
+        $('#snack29name').addClass('active');
+    });
+    $('#snackimg30').click(function(){
+        $('#snack30name').addClass('active');
+    });
+
+});
