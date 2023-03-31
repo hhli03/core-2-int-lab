614e1716073429548e98e0552c42ac09cd30a19b
hhli03 <lik954@newschool.edu>
2023-03-31 17:13:57 -0400

    lab-color-challenge

--- sections/week-8/color.js
+++ sections/week-8/color.js
@@ -1,42 +1,23 @@
+const myList = document.querySelector("div");
+const myRequest = new Request("color.json");
 
-//setup container element
-let container = document.getElementById("container");
+fetch(myRequest)
+  .then((response) => response.json())
+  .then((data) => {
+	console.log(data);
 
-// must setup a local server to use fetch
-// see Python instructions here:
-// https://developer.mozilla.org/en-US/docs/Learn/Common_questions/Tools_and_setup/set_up_a_local_testing_server#using_python
-
-fetch('color.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    processColor(data);
-  })
-  .catch(error => console.log(error));
-
-
-function processColor( data ){
-    let key = parseInt(document.querySelector('div').dataset.colorid );
-    console.log(key);
-
-  data.forEach( function(item, index){
-    // console.log(item, index);
-
-    if ( item.id == key ){
- 
-        let newItem = document.createElement("div");
-        // newItem.style.cssText = `font-size: ${usage}px`;
-        newItem.classList.add('grid');
-        newItem.innerHTML = `
-        <div id="leftbox">
-          <div class="colorname">${item.colorname}</div>
-          <div class="hexcode">${item.hexcode}</div>
-          <div class="color1">${item.color}</div>
-          
-          </div>`;
-
-        container.appendChild(newItem);    
+    for (const color of data.colors) {
+      const newItem = document.createElement("div");
+      newItem.appendChild(document.createElement("strong")).textContent =
+        color.Name;
+      newItem.append(`${color.Hexcode} `);
+      newItem.appendChild(
+        document.createElement("strong")
+      ).style.background = `${color.Hexcode}`;
+      newItem.append(`${color.Colorname} `);
+      myList.appendChild(newItem);      
     }
-});
-}
 
+
+  })
+  .catch(console.error);
