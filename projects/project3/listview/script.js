1ef33fd5b8d7158b4562a47c5187c2d9b4e33e0c
hhli03 <lik954@newschool.edu>
2023-04-20 12:41:07 -0400

    reading12

--- projects/project3/listview/script.js
+++ projects/project3/listview/script.js
@@ -1,51 +1,66 @@
-console.log('loaded');
+function toggleDropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    document.getElementById("rankbymyDropdown").classList.toggle("rankbyshow");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+function toggleRankByDropdown() {
+  document.getElementById("rankbymyDropdown").classList.toggle("rankbyshow");
+}
 
-              if (!event.target.matches('.rankbydropbtn')) {
-      var dropdowns = document.getElementsByClassName("rankbydropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('rankbyshow')) {
-          openDropdown.classList.remove('rankbyshow');
-        }
-        }
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
 
-
-  
-  function myFunction() {
-    document.getElementById("rankbymyDropdown").classList.toggle("rankbyshow");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.rankbydropbtn')) {
-      var dropdowns = document.getElementsByClassName("rankbydropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('rankbyshow')) {
-          openDropdown.classList.remove('rankbyshow');
-        }
+  if (!event.target.matches('.rankbydropbtn')) {
+    var rankbyDropdowns = document.getElementsByClassName("rankbydropdown-content");
+    for (var j = 0; j < rankbyDropdowns.length; j++) {
+      var openRankByDropdown = rankbyDropdowns[j];
+      if (openRankByDropdown.classList.contains('rankbyshow')) {
+        openRankByDropdown.classList.remove('rankbyshow');
       }
     }
   }
+}
 
-    }}
+function processBabynameData(data){
+  // Filter the data array to only include names from 2019
+  data = data.filter(obj => obj.yr === 2019);
+
+  // Get the maximum value of the cnt property in the filtered data array
+  let maxCnt = Math.max(...data.map(obj => obj.cnt));
+
+  // Get the container element where the names will be displayed
+  let namesContainer = document.getElementById('names-container');
+  
+  // Clear the container element
+  namesContainer.innerHTML = '';
+
+  // Iterate over the filtered array of objects and process each object
+  for (let i = 0; i < data.length; i++) {
+    let babyname = data[i].nm;
+    let cnt = data[i].cnt;
+    let ratio = cnt / maxCnt;
+    let fontWeight = ratio * (800-200) + 200; // calculate font weight based on count
+    let ethnicity = data[i].ethcty;
+    let rnk = data[i].rnk; // add 'rnk' property
+
+    let color = getColorForEthnicity(ethnicity);
+
+    // Create a new element for the name
+    let nameElement = document.createElement('div');
+    nameElement.classList.add('name');
+    nameElement.innerText = babyname;
+    nameElement.setAttribute('data-rnk', rnk); // store 'rnk' value as a data attribute
+    nameElement.style.setProperty('--wght', fontWeight);
+    nameElement.style.color = color;
+
+    // Add the name element to the container
+    namesContainer.appendChild(nameElement);
+  }
+}
--- projects/project3/script.js
+++ projects/project3/script.js
@@ -1,50 +1,86 @@
 console.log('loaded');
 
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
+
+function getColorForEthnicity(ethcty) {
+  switch (ethcty) {
+    case 'HISPANIC':
+      return '#FAD281';
+    case 'BLACK NON HISPANIC':
+      return '#F08584'; 
+    case 'ASIAN AND PACIFIC ISLANDER':
+      return '#94CFB7';
+    case 'WHITE NON HISPANIC':
+      return '#B0B8D7'; 
+    default:
+      return '#808080'; // gray
+  }
+}
+
+function processBabynameData(data){
+  console.log(data);
+
+  // Get the maximum value of the cnt property in the data array
+  let maxCnt = Math.max(...data.map(obj => obj.cnt));
+
+  // Iterate over the array of objects and process each object
+  for (let i = 0; i < data.length; i++) {
+    let babyname = data[i].nm;
+    let cnt = data[i].cnt;
+    let ratio = cnt / maxCnt;
+    let fontWeight = ratio * (800-200) + 200; // calculate font weight based on count
+    let ethnicity = data[i].ethcty;
+    let rnk = data[i].rnk; // add 'rnk' property
+
+    let color = getColorForEthnicity(ethnicity);
+
+    // Update the name element with the current baby name and 'rnk' value
+    let nameElement = document.getElementById('name');
+    nameElement.innerText = babyname;
+    nameElement.setAttribute('data-rnk', rnk); // store 'rnk' value as a data attribute
+
+    // Update the font weight and color of the name element
+    nameElement.style.setProperty('--wght', fontWeight);
+    nameElement.style.color = color;
+  }
+}
 
 
+function generateRandomName() {
   fetch('https://data.cityofnewyork.us/resource/25th-nujf.json?brth_yr=2019')
   .then(response => response.json())
   .then(data => {
-    console.log(data);
-    processBabynameData(data);
-  })
-  .catch(error => console.log(error));
+      let randomIndex = Math.floor(Math.random() * data.length);
+      let randomObject = data[randomIndex];
+      let babyname = randomObject.nm;
+      let cnt = randomObject.cnt;
+      let maxCnt = Math.max(...data.map(obj => obj.cnt));
+      let ratio = cnt / maxCnt;
+      let fontWeight = ratio * (800-200) + 300; // calculate font weight based on count
+
+      let nameElement = document.getElementById('name');
+      nameElement.innerText = babyname;
+
+      nameElement.style.setProperty('--wght', fontWeight);
+      nameElement.style.color = getColorForEthnicity(randomObject.ethcty);
+
+  })}
 
 
-
-function processBabynameData(data){
-     console.log(data);
-     let babyname = data["nm"];
-
-     document.getElementById('name').innerText = babyname;
-
-
-
-    let fontWeight = babyname*10;// wght, goes from 200 to 800 — font weight
-    console.log(fontWeight);
-
- 
-
-
-    let element = document.querySelector('name');
-    element.style.setProperty('--wght', fontWeight);
-
-
-}
+  
